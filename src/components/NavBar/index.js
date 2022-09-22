4c1830f48320b71c32119e23b788845da8719c8d
sivaprasad <nvssprasad94@gmail.com>
2022-09-22 18:06:35 +0000

    memoryApp

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,23 @@
+import './index.css'
+
+const NavBar = props => {
+  const {currentScore, isGameInProgress, topScore} = props
+
+  return (
+    <nav className="nav-bar-container">
+      <div className="title-with-score-container">
+        <div className="logo-and-title-container">
+          <h1 className="title">Memory Challange</h1>
+        </div>
+        {isGameInProgress && (
+          <div className="scores-container">
+            <p className="score">Score: {currentScore}</p>
+            <p className="score">Top Score: {topScore}</p>
+          </div>
+        )}
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
